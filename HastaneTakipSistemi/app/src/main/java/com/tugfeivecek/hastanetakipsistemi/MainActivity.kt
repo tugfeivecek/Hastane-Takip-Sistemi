390dc5fb3fecff988e0e9857eb946d1a25790847
Emre USTA <emre.usta@stu.fsm.edu.tr>
2021-06-27 15:08:22 +0300

    Personel tarafi guncellemeleri yapildi

--- HastaneTakipSistemi/app/src/main/java/com/tugfeivecek/hastanetakipsistemi/MainActivity.kt
+++ HastaneTakipSistemi/app/src/main/java/com/tugfeivecek/hastanetakipsistemi/MainActivity.kt
@@ -13,23 +13,15 @@ import com.tugfeivecek.hastanetakipsistemi.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var drawerLayout: DrawerLayout
-    lateinit var appBarConfiguration: AppBarConfiguration
-    private var permissionControl = 0
-    lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        //bottoönavigation gecisleri
-        //bottom menudeki labelı direk toolbar olarak yazdırma
-        /*val appBarConfiguration = AppBarConfiguration(setOf(R.id.mainFragment,R.id.favoriteFragment,R.id.reservationFragment,R.id.profileFragment,R.id.menuFragment))
-        setupActionBarWithNavController(navController,appBarConfiguration)*/
-        //navigation upp button
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomBarMain)
         val navController = findNavController(R.id.fragmentNavHost)
         bottomNavigationView.setupWithNavController(navController)
+
 
     }
 }
